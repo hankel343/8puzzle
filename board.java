38e2f6d8c6a8bc3d1ab10f6e73528cafa3b2caf4
Hankel Haldin <hankel343@gmail.com>
2022-05-31 12:28:06 -0500

    Assign A Twin Board in Ctor of Board.java

--- board.java
+++ board.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class Board {
     private final int[][] board;
-    private Board twin;
+    private Board twinBoard;
     private int N; //Linear dimension of the board
 
     // create a board from an n-by-n array of tiles,
@@ -13,7 +13,6 @@ public class Board {
 
         /* Init boards */
         board = new int[N][N];
-        twin = null;
 
         /* Create copy of ctor argument */
         for (int i = 0; i < N; i++) {
@@ -21,6 +20,8 @@ public class Board {
                 board[i][j] = tiles[i][j];
             }
         }
+
+        twinBoard = twin();
     }
 
     // string representation of this board
@@ -150,28 +151,24 @@ public class Board {
                     if (i > 0) { // Row above
                         neighbor = new Board(board);
                         neighbor.swap(i, j, i - 1, j);
-                        neighbor.twin();
                         boardStack.push(neighbor);
                     }
 
                     if (i < N - 1) { // Row below
                         neighbor = new Board(board);
                         neighbor.swap(i, j, i + 1, j);
-                        neighbor.twin();
                         boardStack.push(neighbor);
                     }
 
                     if (j > 0) { // Left column
                         neighbor = new Board(board);
                         neighbor.swap(i, j, i, j - 1);
-                        neighbor.twin();
                         boardStack.push(neighbor);
                     }
 
                     if (j < N - 1) { // Right column
                         neighbor = new Board(board);
                         neighbor.swap(i, j, i, j + 1);
-                        neighbor.twin();
                         boardStack.push(neighbor);
                     }
                     break;
@@ -183,10 +180,10 @@ public class Board {
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        if (twin != null)
-            return twin;
+        if (twinBoard != null)
+            return twinBoard;
 
-        Board twinBoard = new Board(board);
+        Board newTwin = new Board(board);
         int i = StdRandom.uniform(N), j = StdRandom.uniform(N);
         int n = StdRandom.uniform(N), m = StdRandom.uniform(N);
         while (twinBoard.tileAt(i, j) == 0 || twinBoard.tileAt(n, m) == 0) {
@@ -197,8 +194,8 @@ public class Board {
         }
 
         twinBoard.swap(i, j, n, m);
-        twin = twinBoard;
-        return twin;
+        twinBoard = newTwin;
+        return twinBoard;
     }
 
     // Helper function for neighbors()
